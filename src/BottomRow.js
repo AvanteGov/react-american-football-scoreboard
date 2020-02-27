7c6054fca05ba10c0455509f1b5fb30efda0d610
Alex <alexcouts18@gmail.com>
2020-02-26 21:34:11 -0500

    completed stretch goals

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+
+
 const BottomRow = () => {
+
+  const [gameDown, setGameDown] = useState(0);
+  const [gameQuarter, setGameQuarter] = useState(0);
+
   return (
+
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{gameDown}</div>
+        <button className="controlButtons__downAdvance" onClick={ () => {
+          if (gameDown < 4) {
+            setGameDown(gameDown + 1)
+          } else {
+            setGameDown(0)
+          }
+        }}>Advance Down</button>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -18,7 +32,14 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{gameQuarter}</div>
+        <button className="controlButtons__quarterAdvance" onClick={ () => {
+          if (gameQuarter < 4) {
+            setGameQuarter(gameQuarter + 1)
+          } else {
+            setGameQuarter(0)
+          }
+        }}>Advance Quarter</button>
       </div>
     </div>
   );
